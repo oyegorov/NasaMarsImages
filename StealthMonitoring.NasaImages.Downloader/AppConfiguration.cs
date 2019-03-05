a6d7dfa7162d2a845a11112db6968d38c70059eb
Oleg Yegorov <yegorov.oleg@gmail.com>
2019-03-05 16:03:43 +0100

    Small refactoring

---
+++ StealthMonitoring.NasaImages.Downloader/AppConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace StealthMonitoring.NasaImages.Downloader
+{
+    public static class AppConfiguration
+    {
+        private static readonly IConfigurationRoot Config = BuildConfig();
+        private static object _lockObject = new object();
+        private static string _imageGalleryPath;
+        
+        public static string ImageGalleryPath { get; } = GetImageGalleryPath();
+
+        private static IConfigurationRoot BuildConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            return configuration;
+        }
+
+        private static string GetImageGalleryPath()
+        {
+            if (_imageGalleryPath != null)
+                return _imageGalleryPath;
+
+            string imageGalleryPath;
+            lock (_lockObject)
+            {
+                if (_imageGalleryPath != null)
+                    return _imageGalleryPath;
+
+                imageGalleryPath = Config["imageGalleryPath"];
+                if (String.IsNullOrEmpty(imageGalleryPath))
+                {
+                    imageGalleryPath = Path.Combine(Path.GetTempPath(), "MarsImages");
+                    Directory.CreateDirectory(imageGalleryPath);
+                }
+
+            }
+
+            _imageGalleryPath = imageGalleryPath;
+            return _imageGalleryPath;
+        }
+    }
+}
--- StealthMonitoring.NasaImages.Downloader/Program.cs
+++ StealthMonitoring.NasaImages.Downloader/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 using StealthMonitoring.NasaImages.Api;
 using StealthMonitoring.Storage;
 
@@ -14,17 +9,11 @@ namespace StealthMonitoring.NasaImages.Downloader
     {
         static async Task<int> Main(string[] args)
         {
-            var config = BuildConfig();
             string datesFileName = args.Length > 0 ? args[0] : "dates.txt";
 
             try
             {
-                var imageRepository = new FileSystemImageRepository(GetImageGalleryPath(config));
-                var apiClient = new MarsRoverApiClient();
-                var nasaImageManager = new NasaImageManager(imageRepository, apiClient);
-
-                nasaImageManager.ClearLocalImages();
-                await nasaImageManager.DownloadImagesAsync(new FileDateListProvider(datesFileName)).ConfigureAwait(false);
+                await DownloadImages(datesFileName).ConfigureAwait(false);
             }
             catch (Exception e)
             {
@@ -35,27 +24,14 @@ namespace StealthMonitoring.NasaImages.Downloader
             return 0;
         }
 
-        private static IConfigurationRoot BuildConfig()
+        private static async Task DownloadImages(string datesFileName)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var imageRepository = new FileSystemImageRepository(AppConfiguration.ImageGalleryPath);
+            var apiClient = new MarsRoverApiClient();
+            var nasaImageManager = new NasaImageManager(imageRepository, apiClient);
 
-            IConfigurationRoot configuration = builder.Build();
-
-            return configuration;
-        }
-
-        private static string GetImageGalleryPath(IConfigurationRoot config)
-        {
-            string imageGalleryPath = config["imageGalleryPath"];
-            if (String.IsNullOrEmpty(imageGalleryPath))
-            {
-                imageGalleryPath = Path.Combine(Path.GetTempPath(), "MarsImages");
-                Directory.CreateDirectory(imageGalleryPath);
-            }
-
-            return imageGalleryPath;
+            nasaImageManager.ClearLocalImages();
+            await nasaImageManager.DownloadImagesAsync(new FileDateListProvider(datesFileName)).ConfigureAwait(false);
         }
     }
 }
